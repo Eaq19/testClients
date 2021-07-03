e89c60a64e9671dcb899f98a61efd4dc26356180
Edison <eandresk.13@hotmail.com>
2021-07-03 12:12:14 -0500

    Se agrega estructura de carpetas main

---
+++ src/main/java/com/corp/testclients/TestclientsApplication.java
@@ -0,0 +1,13 @@
+package com.corp.testclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestclientsApplication.class, args);
+	}
+
+}
